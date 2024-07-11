14ef61742e49ee7c93ef86ca815de907b2539faa
benbu <bugra52135@gmail.com>
2024-07-11 17:58:47 +0300

    yeni eklenen dosya

---
+++ src/metodlar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class metodlar {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            seslen();
+            }
+        public static void seslen(){
+            System.out.println("hey");
+            System.out.println("nasılsın");
+            int a=4;
+            a=3*a;
+            System.out.println("A:" + a);
+
+
+        }
+        }
+
+
+
