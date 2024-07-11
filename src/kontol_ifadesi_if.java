a1d799a9fb9d66845cacd2b917a1228fe176b5f1
benbu <bugra52135@gmail.com>
2024-07-11 15:57:12 +0300

    fırst commıt

---
+++ src/kontol_ifadesi_if.java
@@ -0,0 +1,23 @@
+public class kontol_ifadesi_if {
+    public static void main(String args[]) {
+
+        int secenek;
+        int a, b;
+        secenek = 0;
+        a = 5;
+        b = 7;//
+        //
+        if (secenek == 0) {
+            System.out.println("a+b=" + (a + b));
+        }
+        if (secenek == 1) {
+            System.out.println("a-b=" + (a - b));
+
+        }
+        if (secenek == 2) {
+            System.out.println("a*b=" + (a * b));
+
+        }
+    }
+}
+
---
+++ src/str_ve_4_islem.java
@@ -0,0 +1,14 @@
+public class str_ve_4_islem {
+    public static void main(String args[])
+    {
+        String isim ="bugra";
+    System.out.println("merhaba "+ isim );
+    System.out.println("nasılsın "+ isim +"?" );
+    System.out.println("ne yapıyorsun "+ isim + "?" );
+    int a=1;
+    int b=2;
+    int c=(((a+2)*b) + (a*(b+3)))*2/2;
+    System.out.println(c);
+
+    }
+}
